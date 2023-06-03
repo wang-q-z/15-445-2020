4928370d540d482f97a89103926a298bfbd27336
wang-q-z <2415767706@qq.com>
2023-06-04 02:05:21 +0800

    Changes

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -42,14 +42,72 @@ Page *BufferPoolManager::FetchPageImpl(page_id_t page_id) {
   // 2.     If R is dirty, write it back to the disk.
   // 3.     Delete R from the page table and insert P.
   // 4.     Update P's metadata, read in the page content from disk, and then return a pointer to P.
-  return nullptr;
+    std::lock_guard<std::mutex> lock(latch_);
+
+    if(page_table_.find(page_id) != page_table_.end()){
+        frame_id_t frame_id = page_table_[page_id];
+        Page *re_page = &pages_[frame_id];
+        replacer_->Pin(frame_id);
+        re_page->pin_count_++;
+        return re_page;
+    }
+    // free list
+    frame_id_t frame_id_;
+    if(!free_list_.empty()){
+        frame_id_ = free_list_.front();
+        free_list_.pop_front();
+    }else{
+        if(!replacer_->Victim(&frame_id_)) {
+            return nullptr;
+        }
+    }
+    Page *re_page_ = &pages_[frame_id_];
+
+    if(re_page_->IsDirty()){
+        disk_manager_->WritePage(re_page_->page_id_, re_page_->data_);
+        re_page_->is_dirty_ = false;
+    }
+    page_table_.erase(re_page_->page_id_);
+    page_table_[re_page_->page_id_] = frame_id_;
+
+    re_page_->ResetMemory();
+    disk_manager_->ReadPage(re_page_->page_id_,re_page_->data_);
+    return re_page_;
 }
 
-bool BufferPoolManager::UnpinPageImpl(page_id_t page_id, bool is_dirty) { return false; }
+bool BufferPoolManager::UnpinPageImpl(page_id_t page_id, bool is_dirty) {
+    std::lock_guard<std::mutex> lock(latch_);
+    if (page_table_.find(page_id) == page_table_.end()){
+        return false;
+    }
+    frame_id_t frame_id = page_table_[page_id];
+    Page *u_page = &pages_[frame_id];
+
+    if (u_page->pin_count_ == 0){
+        return false;
+    }
+
+    u_page->pin_count_--;
+    if(u_page->pin_count_ == 0){
+        replacer_->Unpin(frame_id);
+    }
+
+    if (is_dirty){
+        u_page->is_dirty_ = true;
+    }
+    return true;
+}
 
 bool BufferPoolManager::FlushPageImpl(page_id_t page_id) {
   // Make sure you call DiskManager::WritePage!
-  return false;
+  if (page_table_.find(page_id) == page_table_.end()){
+      return false;
+  }
+  frame_id_t frame_id = page_table_[page_id];
+  Page* f_page = &pages_[frame_id];
+
+  disk_manager_->DiskManager::WritePage(page_id,f_page->data_);
+  return true;
 }
 
 Page *BufferPoolManager::NewPageImpl(page_id_t *page_id) {
@@ -58,7 +116,31 @@ Page *BufferPoolManager::NewPageImpl(page_id_t *page_id) {
   // 2.   Pick a victim page P from either the free list or the replacer. Always pick from the free list first.
   // 3.   Update P's metadata, zero out memory and add P to the page table.
   // 4.   Set the page ID output parameter. Return a pointer to P.
-  return nullptr;
+    std::lock_guard<std::mutex> lock(latch_);
+    frame_id_t frame_id_;
+    if(!free_list_.empty()){
+        frame_id_ = free_list_.front();
+        free_list_.pop_front();
+    }else{
+        if(!replacer_->Victim(&frame_id_)) {
+            return nullptr;
+        }
+    }
+
+    *page_id = disk_manager_->AllocatePage();
+    Page* re_page_ = &pages_[frame_id_];
+    re_page_->page_id_ = *page_id;
+    if(re_page_->IsDirty()){
+        disk_manager_->WritePage(re_page_->page_id_, re_page_->data_);
+        re_page_->is_dirty_ = false;
+    }
+    page_table_.erase(re_page_->page_id_);
+    page_table_[re_page_->page_id_] = frame_id_;
+
+    re_page_->ResetMemory();
+    disk_manager_->ReadPage(re_page_->page_id_,re_page_->data_);
+
+    return re_page_;
 }
 
 bool BufferPoolManager::DeletePageImpl(page_id_t page_id) {
@@ -67,11 +149,33 @@ bool BufferPoolManager::DeletePageImpl(page_id_t page_id) {
   // 1.   If P does not exist, return true.
   // 2.   If P exists, but has a non-zero pin-count, return false. Someone is using the page.
   // 3.   Otherwise, P can be deleted. Remove P from the page table, reset its metadata and return it to the free list.
-  return false;
+    std::lock_guard<std::mutex> lock(latch_);
+    if (page_table_.find(page_id) == page_table_.end()){
+        return true;
+    }
+    frame_id_t frame_id = page_table_[page_id];
+    Page* d_page = &pages_[frame_id];
+    if(d_page->pin_count_ != 0){
+        return false;
+    };
+
+    d_page->ResetMemory();
+    page_table_.erase(page_id);
+    disk_manager_->DiskManager::DeallocatePage(page_id);
+    free_list_.push_back(page_id);
+    return true;
 }
 
 void BufferPoolManager::FlushAllPagesImpl() {
-  // You can do it!
+    std::lock_guard<std::mutex> lock(latch_);
+    for (size_t i = 0; i < pool_size_; i++) {
+        // FlushPageImpl(i); // 这样写有问题，因为FlushPageImpl传入的参数是page id，其值可以>=pool size
+        Page *page = &pages_[i];
+        if (page->page_id_ != INVALID_PAGE_ID && page->IsDirty()) {
+            disk_manager_->WritePage(page->page_id_, page->data_);
+            page->is_dirty_ = false;
+        }
+    }
 }
 
 }  // namespace bustub
--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -10,20 +10,62 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <iostream>
 #include "buffer/lru_replacer.h"
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) :capacity(num_pages){}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+/*
+ * Remove the object that was accessed the least recently compared to all the elements being tracked by the Replacer,
+ * store its contents in the output parameter and return True. If the Replacer is empty return False
+ */
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+    latch.lock();
+    if(lru_list.empty()){
+        return false;
+    }
+    frame_id_t frame_temp = lru_list.back();
+    *frame_id = frame_temp;
+    lru_list.pop_back();
+    lruMap.erase(frame_temp);
+    latch.unlock();
+    return true;
+}
+/*This method should be called after a page is pinned to a frame in the BufferPoolManager.
+ * It should remove the frame containing the pinned page from the LRUReplacer
+ */
+void LRUReplacer::Pin(frame_id_t frame_id) {
+    latch.lock();
+    if(lruMap.count(frame_id) != 0){
+        lru_list.remove(frame_id);
+        lruMap.erase(frame_id);
+    }
+    latch.unlock();
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+/*
+ * This method should be called when the pin_count of a page becomes 0.
+ * This method should add the frame containing the unpinned page to the LRUReplacer
+ */
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+    latch.lock();
+    if(lruMap.find(frame_id) == lruMap.end()){
+        if(lru_list.size() >= capacity){
+            frame_id_t last = lru_list.back();
+            lru_list.pop_back();
+            lruMap.erase(last);
+        }
+        lru_list.push_front(frame_id);
+        lruMap[frame_id] = lru_list.begin();
+    }
+    latch.unlock();
+}
 
-size_t LRUReplacer::Size() { return 0; }
+size_t LRUReplacer::Size() { return lru_list.size(); }
 
 }  // namespace bustub
